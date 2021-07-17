017c53cdd4bf46ec48c2a32ab6b784a9fd2e4b44
chim <zatout.chayma@gmail.com>
2021-07-17 16:06:41 +0100

    Variables added to customize the simulator.

--- main.py
+++ main.py
@@ -8,16 +8,20 @@ Desc:
 import open3d as o3d
 from simulator import BASISR55
 
-
 if __name__ == '__main__':
     # visualization:
     vis = o3d.visualization.Visualizer()
-    vis.create_window("BASSAR")
+    vis.create_window("BASISR")
     vis.get_render_option().background_color = [0.75, 0.75, 0.75]
     vis.get_render_option().mesh_show_back_face = True
 
     # create BASISR and add it to the visualizer:
-    basisr = BASISR55()
+    basisr = BASISR55(size=1.5,  # the base size : size x size
+                      pins_per_line=5,  # the number of pins per line (colomn)
+                      pins_R=0.15,  # the pins R (R = r*2)
+                      base_height=0.75,  # the base height
+                      pins_height=0.02
+                      )
     vis.add_geometry(basisr.base)
 
     # For debuging:
@@ -26,19 +30,27 @@ if __name__ == '__main__':
     ################################
     # Uncomment one line to display one of the shapes:
     ################################
-    # basisr.chair(height=5)
-    # basisr.table(height=5)
-    # basisr.dresser(height=5)
-    # basisr.window(height=5)
-    # basisr.door(height=5)
-    # basisr.upstairs(height=5)
-    # basisr.downstairs(height=5)
-    # basisr.bathtubs(height=5)
+    # basisr.chair(height=3)
+    # basisr.table(height=3)
+    # basisr.dresser(height=3)
+    # basisr.window(height=3)
+    # basisr.door(height=3)
+    # basisr.upstairs(height=3)
+    # basisr.downstairs(height=3)
+    # basisr.bathtubs(height=3)
+
+    ################################
+    # Uncomment one line to display different heights:
+    ################################
+    # Vosualize different heights:
+    # basisr.update_pin(0, 0, 1)
+    # basisr.update_pin(0, 4, 2)
+    # basisr.update_pin(4, 0, 3)
 
     # Add pins to visualizer:
     for j in range(basisr.pins_per_line):
         for i in range(basisr.pins_per_line):
-                vis.add_geometry(basisr.pins[j][i])
+            vis.add_geometry(basisr.pins[j][i])
 
     # Display window:
     vis.run()
